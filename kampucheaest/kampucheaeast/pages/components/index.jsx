e3fef06488c4d3ca961c61aab81940d304e874e1
nhokphal <phalnhok@gmail.com>
2023-01-23 21:55:18 +0700

    first commit

---
+++ kampucheaest/kampucheaeast/pages/components/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import About from './about';
+
+
+const container = styled.div
+`
+
+`;
+const index = () => {
+  return (
+    <container>
+    <div>
+    <About/>
+    </div>
+    </container>
+  )
+}
+
+export default index
