2371c98a85faf8b12f45b6d3581b4f02f213a209
nhokphal <phalnhok@gmail.com>
2023-01-27 00:43:04 +0700

    update

--- kampucheaest/kampucheaeast/components/footer.jsx
+++ kampucheaest/kampucheaeast/components/footer.jsx
@@ -4,53 +4,121 @@ import styled from 'styled-components'
 
 const Container = styled.div
 `
-  margin: 10px;
-  padding: 10px;
   width: 1440;
+  font-family: 'Roboto';
+  position: relative;
+  display: flex;
   height: 410px;
+  min-height: 150px;
+// `;
+  const Wrapper = styled.div
+  `
+  padding: 50px;
+  margin: auto;
+  `;
+
+  // const LineDivider = styled.div
+  // `
+  // border: 0.1px solid #000000;
+  // background-color: red;
+  // margin: 10px;
+  // top: 1%;
+  // `;
+
+  const TextLogo = styled.div
+  `
+  font-family: 'Roboto';
+  background-color: yellow;
+  color: #000000;
+  width: 5px;
+  padding-top: 1px;
+  text-transform: uppercase;
+  `;
+
+
+const TextParagraph = styled.div
+`
+  font-size: 13px;
+  font-weight: 10px;
+  left: 20%;
+  right: 53.82%;
+  top: 17.53%;
+  bottom: 40.5%;
+`;
+
+const TextProduct = styled.div
+`
+line-height: 1.5;
+`;
+const TextProductParent = styled.div
+`
+  display: flex;
+  gap: 50%;
+  justify-content: space-between;
+  font-size: 13px;
+
+  `;
+
+  const TextCopyRight = styled.div
+  `
   position: absolute;
-`;
+  bottom: 5px;
+  width: 200px;
+  background-color: white;
+  font-size: 13px;
+  padding-bottom: 1px;
 
-const LineDivider = styled.div
-`
-border: 1px solid #000000;
-right: 50%;
-left: 50%;
-top: 7.89%;
-bottom: 7.89%;
-`;
-
-const TextLogo = styled.p
-`
-font-family: 'Roboto';
-left: 9%;
-right: 85%;
-top: 15.53%;
-bottom: 76.52%;
-text-align: center;
-color: #000000;
-text-transform: uppercase;
-`;
-
-
-const TextParagraph = styled.p
-`
-
-`;
+  `;
 
 const Footer = () => {
   return (
     <Container>
-      <TextLogo>
-        Kampucheaest
-      </TextLogo>
-    <TextParagraph>
-    <h3>Ready to get started?</h3>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit,<br/> 
-      sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. <br/>
-       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-      <br/>ut aliquip ex ea commodo consequat.
-    </TextParagraph>
+    <Wrapper>
+    <TextLogo>
+          Kampucheaest
+      </TextLogo>  
+    </Wrapper>
+    <Wrapper>
+      <TextParagraph>
+      
+      <h3>Ready to get started?</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit<br/> 
+        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua
+      </p>
+      </TextParagraph>
+    </Wrapper>
+    <Wrapper>
+      <TextProductParent>
+      <TextProduct>
+         <p>
+          <b>Product</b>
+          <br/>
+          Product
+          <br/>
+          Product
+          <br/>
+          Product
+          </p> 
+      </TextProduct>     
+      <TextProduct>
+         <p>
+          <b>Company</b>
+          <br/>
+          Company
+          <br/>
+          Company
+          <br/>
+          Company
+          </p> 
+      </TextProduct>
+      </TextProductParent>
+      <TextCopyRight>
+      © 2010 — 2020  Privacy — Terms
+      </TextCopyRight>
+    </Wrapper>
+    <Wrapper>
+    </Wrapper>
     </Container>
   
   )
