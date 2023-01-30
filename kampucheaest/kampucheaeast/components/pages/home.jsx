fda32d7930a3d066e930380584987f7951e23698
nhokphal <phalnhok@gmail.com>
2023-01-30 22:10:33 +0700

    update route home

--- kampucheaest/kampucheaeast/components/pages/home.jsx
+++ kampucheaest/kampucheaeast/components/pages/home.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import Link from 'next/link';
+import styled from 'styled-components';
+import { useRouter } from 'next/router'
 
 const Home = () => {
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
+  const RedLink = styled.a`
+  color: blue;
+  
+` 
+const Active = styled.a
+` color: black;
+`;
+const NonActive = styled.a
+`
+ color: red;
+`;
+
   return (
     <div>
-    Home
+    <Link href="/" passHref legacyBehavior>
+    <RedLink className={currentRoute == "/" ? <RedLink/> : <NonActive/>}>
+      Home
+    </RedLink>
+    </Link>
     </div>
   )
 }
