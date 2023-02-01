31c7f3643b1d32dd8bc6a20901e1f52daf6ed269
nhokphal <phalnhok@gmail.com>
2023-02-02 00:19:03 +0700

    update contact

--- kampucheaest/kampucheaeast/pages/contact.js
+++ kampucheaest/kampucheaeast/pages/contact.js
@@ -1,10 +1,40 @@
 import React from 'react'
+import styled from 'styled-components';
+
+
+const Container = styled.div`
+padding: 10px;
+`;
+
+const Wrapper = styled.div`
+width: 1440px;
+height: 1274px;
+justify-content: center;
+display: flex;
+`;
+
+const ContactCard = styled.text`
+font-size: 25px;
+font-family: 'roboto';
+`;
+
+const Paragraph = styled.text`
+flex: none;
+`;
+
 
 const Contact = () => {
   return (
-    <div>
-    Contact
-    </div>
+    <Container>
+    <Wrapper>
+    <ContactCard>
+        Contact Us 
+    </ContactCard>
+      <Paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+      </Paragraph>  
+    </Wrapper>
+    </Container>
   )
 }
 
